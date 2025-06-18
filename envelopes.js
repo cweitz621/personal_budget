d89f36224b67a1f738db7d1f40c6b23c236ac539
Collin Weitz <collinweitz@Collins-MacBook-Air.local>
2025-06-18 19:37:53 -0400

    adding post requests and initial test envelope db

---
+++ envelopes.js
@@ -0,0 +1,8 @@
+const envelopes = [
+  { id: 1, category: 'Groceries', budget: 300 },
+  { id: 2, category: 'Rent', budget: 1200 },
+  { id: 3, category: 'Utilities', budget: 200 }
+];
+
+module.exports = envelopes;
+
--- server.js
+++ server.js
@@ -2,12 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const envelopes = require('./envelopes');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// GET ROUTES
 
 app.get('/', (req, res, next) => {
     res.send('hello world')
 })
+
+// POST ROUTES
+
+app.post('/envelopes', (req, res, next) => {
+    const { category, budget } = req.body;
+    const numericBudget = Number(budget);
+
+    if (typeof category === 'string' && !isNaN(numericBudget)) {
+        const maxId = envelopes.reduce((max, env) => Math.max(max, env.id), 0);
+        const newEnvelope = {
+            id: maxId + 1,
+            category,
+            budget: numericBudget
+        };
+
+        envelopes.push(newEnvelope);
+        res.status(201).send(newEnvelope);
+    } else {
+        res.status(400).send('Invalid request body');
+    }
+});
+
+
+// PUT ROUTES
+
+// DELETE ROUTES
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
